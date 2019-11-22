af608fd3539987f93eaf2c2f6848abb5f7ae0216
Prasanth Baireddy <prasanthbaireddy@mv-pbaireddy-m.lan>
2019-11-21 21:48:39 -0800

    updated AVURL

--- client_test.go
+++ client_test.go
@@ -15,4 +15,49 @@ func TestClient_Get_Source(t *testing.T) {
 	x, err := c.GetSource("", "")
 	fmt.Println(x)
 	assert.Equal(t, err, nil)
+
+	/*config := NewConfig("")
+	c := Client{config}
+
+	camStreamConfig, err := c.GetSource("", "")
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if camStreamConfig != "" {
+		var camStmConfig []CameraStreamConfiguration
+
+		err := json.Unmarshal([]byte(camStreamConfig), &camStmConfig)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("CAM STREAM ERROR \n %v", err.Error()))
+		}
+
+		//camStream = append(camStream, camStmConfig[0])
+		endpointCapability := EndpointCapabilities{}
+		endpointCapabilities := []EndpointCapabilities{}
+		endpointCapability.CameraStreamConfigurations = &camStmConfig
+		endpointCapabilities = append(endpointCapabilities, endpointCapability)
+		x, _ := json.Marshal(endpointCapabilities)
+		t.Log("XXX")
+		fmt.Println(string(x))
+		var xa int
+		xa = 5
+		fmt.Println(xa)*/
 }
+
+/*type EndpointCapabilities struct {
+	CameraStreamConfigurations *[]CameraStreamConfiguration `json:"cameraStreamConfigurations,omitempty"`
+}
+
+type CameraStreamConfiguration struct {
+	Protocols          []string     `json:"protocols"`
+	Resolutions        []Resolution `json:"resolutions"`
+	AuthorizationTypes []string     `json:"authorizationTypes"`
+	VideoCodecs        []string     `json:"videoCodecs"`
+	AudioCodecs        []string     `json:"audioCodecs"`
+}
+
+type Resolution struct {
+	Width  int `json:"width"`
+	Height int `json:"height"`
+}*/
--- const.go
+++ const.go
@@ -1,4 +1,4 @@
 package smartthings
 
 //const ST_AV_URL = "https://api.st-av.net/v1"
-const ST_AV_URL = "https://api.d.st-av.net"
+const ST_AV_URL = "https://api.d.st-av.net/v1"
